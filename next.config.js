624f6f0535437aebc2cb8d6eec6f3ac643602f93
James Serengia <jamesserengia@gmail.com>
2023-03-16 10:39:31 +0300

    Update next-config-file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
