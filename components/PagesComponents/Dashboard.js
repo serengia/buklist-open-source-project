5654fb91031504edf936d57835d6c41adf0e2515
James Serengia <jamesserengia@gmail.com>
2023-03-16 13:14:15 +0300

    Add dashbord page

--- components/PagesComponents/Dashboard.js
+++ components/PagesComponents/Dashboard.js
@@ -1,5 +1,14 @@
 import React from "react";
+import s from "./Dashboard.module.scss";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <main>
+      <section>
+        <div className={`${s["dashboard-container"]} row`}>
+          <h1>Dashboard</h1>
+        </div>
+      </section>
+    </main>
+  );
 }
